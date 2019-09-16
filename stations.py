d30f8582afda4fa8a4cfbba70e5f2c8f7bab121d
cd <claudio.donofrio@nateko.lu.se>
2019-09-16 11:13:15 +0200

    clean up

--- stations.py
+++ stations.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 running this script saves a html file in the current
-directory with a map based on leaflet.js. 
-The map contains all ICOS NEON AMERIFLUC and LTER/DEIMS stations
-dynamically loaded from their respective webservices.
+directory with a world map based on leaflet.js. 
+The map contains marker for different research stations
+collecting greenhouse gas measurments and ecological parameters
+to study carbon cycles and soil, ecosystem and atmosphere interactions.
+
+the station information is download dynamically loaded from their
+respective webservices but the generated station map itself is static
+afterwards.
+
+currently the following networks are implemented:
+    ICOS, FLUXNET, AMERIFLUX, NEON, LTER/DEIMS, ASIAFLUX
 
 @author: Claudio D'Onofrio
 """
@@ -15,30 +23,30 @@ import sys
 import helper_functions as h
 
 # set the list of necessary modules to run the code
-modules = ['os', 'webbrowser', 'folium', 'icos', 'neon', 'ameriflux', 'lter', 'fluxnet']
+modules = ['os', 'webbrowser', 'folium', 'tqdm']
+networks = ['icos', 'neon', 'ameriflux', 'lter', 'fluxnet', 'asiaflux']
 
+dependencies = modules + networks
 # check if the modules are available and load them, otherwise stop execution
-if not h.checklib(modules):
+if not h.checklib(dependencies):
     sys.exit('module dependencies are not fulfilled')
 
 else:
     import os
     import webbrowser
-    import folium            
+    import folium    
+    
     import icos
     import neon
+    import asiaflux
+    import fluxnet
     import ameriflux
     import lter
-    import fluxnet
-    import asiaflux
-    from tqdm import tqdm
 
 dbg = True
 
-networklist = {'icos', 'neon','ameriflux','lter','fluxnet','asiaflux'}
-
 mcList = [] # Marker Cluster List
-for n in tqdm(networklist):
+for n in networks:
     # get the marker cluster
     h.debugPrint(dbg, ''.join(['processing ',n]))
     mcList.append(eval(n+'.sites_marker_list()'))
@@ -47,7 +55,6 @@ for n in tqdm(networklist):
 # create a map with all stations
 # define the initinal paramters of centre point and zoom
 h.debugPrint(dbg,'creating map with markers')
-
 
 Center = [43, 5]
 Zoom = 4
@@ -71,21 +78,17 @@ folium.TileLayer('stamenwatercolor').add_to(myMap)
 folium.TileLayer('stamentoner').add_to(myMap)
 folium.TileLayer('stamenterrain').add_to(myMap)
 folium.TileLayer('Mapbox Control Room').add_to(myMap)
-folium.TileLayer('Mapbox Bright').add_to(myMap)
 
 #---------------------------------------------------------------
 # create the feature groups and add control
 # feature groups will be displayed as well in the
 # top right menu. You can easly switch on/off 
 # the different layers.
+h.debugPrint(dbg,'adding feature groups')
 
-#fgList = []
-for i, n in tqdm(enumerate(networklist)):
+for i, n in enumerate(networks):
     f = eval(''.join(['folium.FeatureGroup(name="',n,'")']))
     eval('f.add_child(mcList[i]).add_to(myMap)')
-
-#for i, f in enumerate(fgList):
-#    eval('f.add_child(mcList[i]).add_to(myMap)')
 
 folium.LayerControl().add_to(myMap)
 #---------------------------------------------------------------
@@ -95,10 +98,9 @@ myMap.save(mapname)
 
 url = os.path.abspath(mapname)
 h.debugPrint(dbg,('map saved to: ' + url))
-# open the saved html with default browser
-
 # the html file is now saved to the directory where your code is
-# let's try to open your default browser with the map.
+
+# let's try to open your default browser with the map
 webbrowser.open(url, new=2)
 
 
